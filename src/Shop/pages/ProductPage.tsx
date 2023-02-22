f17c8155214f5d76a984a01b511e2910051ac9fa
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-02-22 20:45:05 +0000

    STYLE - updated nav styling to better fit within the boundaries of the shop item section

--- src/Shop/pages/ProductPage.tsx
+++ src/Shop/pages/ProductPage.tsx
@@ -1,27 +1,14 @@
-import { useEffect } from "react"
 import { useOutletContext, useParams } from "react-router-dom"
 
 export default function ProductPage() {
     const { productId } = useParams()
-    const showProductArr = useOutletContext()
-
-    console.log(showProductArr.showProduct)
-
-    useEffect(() => {
-        showProductArr.setShowProduct(true)
-    }, [])
+    // const showProductArr = useOutletContext()
 
     console.log(productId)
     return (
         <>
             item {productId}
-            <button
-                onClick={() => {
-                    showProductArr.setShowProduct(!showProductArr.showProduct)
-                }}
-            >
-                S H O P
-            </button>
+            <button onClick={() => {}}>S H O P</button>
         </>
     )
 }
