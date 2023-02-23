5d8e97be28bf8ff707b9e54f366bbe8adbb2ce63
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-02-23 11:03:03 +0000

    BUILD - fixed interface for showProduct data

--- src/Shop/pages/ProductPage.tsx
+++ src/Shop/pages/ProductPage.tsx
@@ -1,8 +1,12 @@
 import { Link, useOutletContext, useParams } from "react-router-dom"
 
+interface Product {
+    showProduct: boolean
+    setShowProduct: (arg0: boolean) => void
+}
 export default function ProductPage() {
     const { productId } = useParams()
-    const showProduct = useOutletContext()
+    const showProduct: Product = useOutletContext()
 
     console.log(productId)
     console.log(showProduct)
