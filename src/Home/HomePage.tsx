996f34fbe3a689c43b19519d40cdaecb0599f2aa
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-02-26 21:40:24 +0000

    STYLE - asset added for homepage

--- src/Home/HomePage.tsx
+++ src/Home/HomePage.tsx
@@ -1,5 +1,11 @@
-import homeSCSS from "../style/Home.module.scss"
+import homeSCSS from "./style/Home.module.scss"
 
 export default function Home() {
-    return <>Home</>
+    return (
+        <>
+            <main className={homeSCSS.homeMain}>
+                <section className={homeSCSS.homeSection}></section>
+            </main>
+        </>
+    )
 }
